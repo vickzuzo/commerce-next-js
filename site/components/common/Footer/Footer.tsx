d9a7836641e9116c8b84b58ece4b6b0efd8ad0d2
vickzuzo <65499251+vickzuzo@users.noreply.github.com>
2022-09-07 14:27:19 +0200

    change github url

--- site/components/common/Footer/Footer.tsx
+++ site/components/common/Footer/Footer.tsx
@@ -58,7 +58,7 @@ const Footer: FC<Props> = ({ className, pages }) => {
               <a
                 className={s.link}
                 aria-label="Github Repository"
-                href="https://github.com/vercel/commerce"
+                href="https://github.com/vickzuzo"
               >
                 <Github />
               </a>
